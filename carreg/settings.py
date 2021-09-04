665665e0898cf245794f806d738538bf71f13beb
Ekaterina Premudraya <smolyaevaekaterina@gmail.com>
2021-09-04 16:28:10 +0600

    allowed hosts add

--- carreg/settings.py
+++ carreg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i^p+7)bay+&@2(hnkz(eruiay*#9=v9*k-p_*xt652_)^#iwxm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
